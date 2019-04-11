4905f83247020e1cd7b61acae8ce86569dff8cec
Tim Semenyuk <powertimka@yandex.ru>
2019-04-11 21:09:23 +0300

    build(rollup): add rollup

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/index.js',
+    external: ['ms'],
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+        runtimeHelpers: true,
+      }),
+    ],
+  },
+];
