834fb620dc158be10d4fb41f4698a1285f1279b7
Tim Semenyuk <powertimka@yandex.ru>
2019-04-11 21:01:35 +0300

    docs(example): add simple page example

---
+++ example/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Editor from '../src';
+
+const el = document.getElementById('app');
+
+const wrapperStyle = {
+  width: '1280px',
+  backgroundColor: '#efefef',
+  padding: '20px',
+  border: '1px solid #e0e0e0',
+};
+
+ReactDOM.render(
+  <div style={wrapperStyle}>
+    <Editor excludeDefaultTools={['checklist']} />
+  </div>,
+  el,
+);
