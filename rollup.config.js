213a7035795833bb89f7ca0a235ffe74b1727cbb
Tim Semenyuk <powertimka@yandex.ru>
2019-04-14 20:48:06 +0300

    build(babel): fix cjs build, remove unused deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,36 @@
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
+
+const name = 'ReactEditor.js';
 
 export default [
   {
     input: 'src/index.js',
-    external: ['ms'],
+    external: ['react'],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' },
+      {
+        file: pkg.browser,
+        format: 'iife',
+        name,
+
+        globals: {
+          react: 'React',
+          '@editorjs/editorjs': 'EditorJS',
+        },
+      },
     ],
     plugins: [
       babel({
         exclude: 'node_modules/**',
         runtimeHelpers: true,
+        plugins: [
+          ['transform-react-remove-prop-types', { removeImport: true }],
+        ],
       }),
+      commonjs(),
     ],
   },
 ];
